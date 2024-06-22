32239e09aba66cf53f78d0e73125fe2013518977
A-phenomenal1 <niteshch200053@gmail.com>
2024-06-22 20:14:46 +0530

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import styles from "./app.module.scss";
+
+function App() {
+  return (
+    <main className={styles.container}>
+      <h1>Hello React1</h1>
+    </main>
+  );
+}
+
+export default App;
